44f2262f01c79e7f5d32535921cf94ae3c27f23c
nathanpucheril <nathanpucheril@gmail.com>
2016-05-18 15:58:29 -0600

    Pushing All Updates

--- ProbabilityTable.py
+++ ProbabilityTable.py
@@ -3,10 +3,9 @@
 # @author Nathan Pucheril
 # @author Keith Hardaway
 
-from itertools import product, combinations
+from itertools import product
 from copy import deepcopy, copy
 from warnings import warn
-from functools import reduce
 
 class Factor(object):
     """ Description:
@@ -58,21 +57,30 @@ class Factor(object):
             entree = entrees[0]
             self._table[entree] = val
 
-
     def __str__(self):
         """ Returns human readable Probability Table """
-        longest_var = max(map(len,self._variables))
-        def str_helper(s): return str(s).rjust(longest_var, " ").ljust(longest_var, " ")
-        header = "P({unc}{given}{cond})".format(unc=", ".join(self._unconditioned),
-                                            given=" | " if self._conditioned else "",
-                                            cond=", ".join(self._conditioned))
-        var_header = str_helper("{vars}".format(vars=" | ".join(self._variables)))
-        lst_of_entrees = [" | ".join(map(str_helper, map(lambda x: x[1], k))) + " | " + str(v) for k, v in sorted(self._table.items())]
-        table = "\n".join(lst_of_entrees)
-        return "{header}\n{rule}\n{var_header}\n{table}".format(table=table,
-                                                                header=header,
-                                                                var_header = var_header,
-                                                                rule="-" * len(header))
+        length = len(max(self._variables, key = len))
+        def str_beautify(s): return str(s).rjust(length + 2, " ").ljust(length + 3, " ")
+        probability = "P({unc}{given}{cond})".format(unc=", ".join(self._unconditioned),
+                                                     given=" | " if self._conditioned else "",
+                                                     cond=", ".join(self._conditioned))
+        var_header = str_beautify(" {vars}".format(vars=" | ".join(map(str_beautify,self._variables))))
+        lst_of_entrees = [" | ".join(map(str_beautify, map(lambda x: x[1], k))) + " | " + str(v) for k, v in sorted(self._table.items())]
+        table = " \n ".join(lst_of_entrees)
+        border_size = (len(var_header) + len(probability) + 5)
+        return "{border}\n{var_header} | {probability} \n{rule}\n {table} \n{border}\n\n".format(table=table,
+                                                                    probability=probability,
+                                                                    var_header = var_header,
+                                                                    rule="-" * border_size,
+                                                                    border = "*" * border_size
+                                                                    )
+
+    def __eq__(self, object2):
+        return type(self) == type(object2) and \
+               self._unconditioned == object2._unconditioned and \
+               self._conditioned == object2._conditioned and \
+               self._entrees == object2._entrees and \
+               all([self._table[entree] == object2._table[entree] for entree in self._entrees])
 
     def load_factor(self, string):
         pass
@@ -85,10 +93,11 @@ class JPT(Factor):
     def __init__(self, variables, variable_domains):
         """Initializes a Joint Probability Table """
         super().__init__(variables, set(), variable_domains)
+        if not self.valid_table():
+            warn("JPT is not valid!")
 
     def valid_table(self):
         """Checks if Probability Rules are followed """
-        assert sum(self._table[entree] for entree in self.get_entrees()) == 1, "JPT does not sum to 1"
         return True if sum(self._table[entree] for entree in self.get_entrees()) == 1 else False
 
     def get_probability(self, **variables):
@@ -121,33 +130,31 @@ class CPT(Factor):
             jpt.set_probability(prob, **dict(entree))#shouldnt have to put entree in dict
         return jpt
 
-
-
-
 def variable_elimination(elimination_var, factors):
     pass
 
-# var1 = ["hi", "bye"]
-# dom1 = {"hi": ["friend", "parent"], "bye": ["friend", "parent"]}
-# var2 = ["mom", "dad"]
-# dom2 = {"mom": ["scold", "praise"], "dad": ["scold", "praise"]}
-#
-# cpt1 = CPT("hi", "bye", dom1)
-# entrees = cpt1.get_entrees()
-# # print(entrees)
-# for entree in entrees:
-#     # print(dict(entree))
-#     cpt1.set_probability(.2, **dict(entree))
-#
-# cpt2 = CPT("mom", "dad", dom2)
-# entrees = cpt2.get_entrees()
-# # print(entrees)
-# for entree in entrees:
-#     # print(dict(entree))
-#     cpt2.set_probability(.1, **dict(entree))
-#
-# print(cpt1)
-# print(cpt2)
-#
-# jpt = CPT.cpts2jpt([cpt1, cpt2])
-# print(jpt)
+var1 = ["hi", "bye"]
+dom1 = {"hi": ["friend", "parent"], "bye": ["friend", "parent"]}
+var2 = ["mom", "dad"]
+dom2 = {"mom": ["scold", "praise"], "dad": ["scold", "praise"]}
+
+cpt1 = CPT("hi", "bye", dom1)
+entrees = cpt1.get_entrees()
+# print(entrees)
+for entree in entrees:
+    # print(dict(entree))
+    cpt1.set_probability(.2, **dict(entree))
+
+
+cpt2 = CPT("mom", "dad", dom2)
+entrees = cpt2.get_entrees()
+# print(entrees)
+for entree in entrees:
+    # print(dict(entree))
+    cpt2.set_probability(.1, **dict(entree))
+
+print(cpt1)
+print(cpt2)
+
+jpt = CPT.cpts2jpt([cpt1, cpt2])
+print(jpt)
