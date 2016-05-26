1b013b830f3541d7e886bb07db16578796314b1c
nathanpucheril <nathanpucheril@gmail.com>
2016-05-25 23:30:38 -0700

    Pushing All Updates

--- ProbabilityTable.py
+++ ProbabilityTable.py
@@ -6,6 +6,7 @@
 from itertools import product
 from copy import deepcopy, copy
 from warnings import warn
+import random
 
 class Factor(object):
     """ Description:
@@ -21,8 +22,8 @@ class Factor(object):
         self._variables = set(conditioned_vars) | set(unconditioned_vars)
         self._unconditioned = unconditioned_vars
         self._conditioned = conditioned_vars
-        entry_temp = [[(var, domain) for domain in self._domains[var]] for var in self._variables]
-        self._entries = list(sorted(product(*entry_temp)))
+        entree_temp = [[(var, domain) for domain in self._domains[var]] for var in self._variables]
+        self._entries = list(sorted(product(*entree_temp)))
         self._table = {}
         for entry in self._entries:
             self._table[entry] = 0
@@ -31,9 +32,9 @@ class Factor(object):
     def get_domains(self):
         return deepcopy(self._domains)
 
-    def get_entries(self):
+    def get_entrees(self):
         """ Retrieves all possible entries to the table """
-        return self._entries
+        return deepcopy(self._entries)
 
     def get_probability(self, **variables):
         """ Gets probability of a specific entry in table """
@@ -65,8 +66,8 @@ class Factor(object):
                                                      given=" | " if self._conditioned else "",
                                                      cond=", ".join(self._conditioned))
         var_header = str_beautify(" {vars}".format(vars=" | ".join(map(str_beautify,self._variables))))
-        lst_of_entries = [" | ".join(map(str_beautify, map(lambda x: x[1], k))) + " | " + str(v) for k, v in sorted(self._table.items())]
-        table = " \n ".join(lst_of_entries)
+        lst_of_entrees = [" | ".join(map(str_beautify, map(lambda x: x[1], k))) + " | " + str(v) for k, v in sorted(self._table.items())]
+        table = " \n ".join(lst_of_entrees)
         border_size = (len(var_header) + len(probability) + 5)
         return "{border}\n{var_header} | {probability} \n{rule}\n {table} \n{border}\n\n".format(table=table,
                                                                     probability=probability,
@@ -98,7 +99,7 @@ class JPT(Factor):
 
     def valid_table(self):
         """Checks if Probability Rules are followed """
-        return True if sum(self._table[entry] for entry in self.get_entries()) == 1 else False
+        return True if sum(self._table[entry] for entry in self.get_entrees()) == 1 else False
 
     def get_probability(self, **variables):
         """ Gets probability of certain variables. Includes summing out over vars """
@@ -111,7 +112,7 @@ class JPT(Factor):
 
 class CPT(Factor):
     def __init__(self, unconditioned_var, conditioned_vars, variable_domains):
-        super().__init__(set([unconditioned_var]), conditioned_vars, variable_domains)
+        super().__init__(list([unconditioned_var]), list([conditioned_vars]), variable_domains)
 
     @staticmethod
     def cpts2jpt(cpts):
@@ -120,15 +121,21 @@ class CPT(Factor):
         for cpt in cpts:
             all_domains.update(cpt.get_domains())
         jpt = JPT(all_domains.keys(), all_domains)
-        jpt_entries = jpt.get_entries()
-        for entry in jpt_entries:
+        jpt_entrees = jpt.get_entrees()
+        for entry in jpt_entrees:
             prob = 1
             for cpt in cpts:
-                for cpt_entry in cpt.get_entries():
-                    if set(cpt_entry) | set(entry) == set(entry): # Union of CPT and JPT does not yield new Vars
-                        prob *= cpt.get_probability(**dict(cpt_entry)) #shouldnt have to put entry in dict
+                for cpt_entree in cpt.get_entrees():
+                    if set(cpt_entree) | set(entry) == set(entry): # Union of CPT and JPT does not yield new Vars
+                        prob *= cpt.get_probability(**dict(cpt_entree)) #shouldnt have to put entry in dict
             jpt.set_probability(prob, **dict(entry))#shouldnt have to put entry in dict
         return jpt
+
+    @staticmethod
+    def random_cpt(unconditioned_var, conditioned_vars, variable_domains):
+        r = CPT(unconditioned_var, conditioned_vars, variable_domains)
+
+
 
 def variable_elimination(elimination_var, factors):
     pass
@@ -138,16 +145,16 @@ dom1 = {"hi": ["friend", "parent"], "bye": ["friend", "parent"]}
 var2 = ["mom", "dad"]
 dom2 = {"mom": ["scold", "praise"], "dad": ["scold", "praise"]}
 
-cpt1 = CPT("hi", set(["bye"]), dom1)
-entries = cpt1.get_entries()
+cpt1 = CPT("hi", "bye", dom1)
+entries = cpt1.get_entrees()
 # print(entries)
 for entry in entries:
     # print(dict(entry))
     cpt1.set_probability(.2, **dict(entry))
 
 
-cpt2 = CPT("mom", set(["dad"]), dom2)
-entries = cpt2.get_entries()
+cpt2 = CPT("mom", "dad", dom2)
+entries = cpt2.get_entrees()
 # print(entries)
 count = .01
 for entry in entries:
---
+++ docs/docs/_build/html/_sources/index.txt
@@ -0,0 +1,22 @@
+.. PyBayes documentation master file, created by
+   sphinx-quickstart on Wed May 25 23:29:46 2016.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to PyBayes's documentation!
+===================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
